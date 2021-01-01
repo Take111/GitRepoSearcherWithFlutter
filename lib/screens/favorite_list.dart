e5e828bb374a98d98a0c04373449d0549f93029f
Take111 <maxmaxastea@gmail.com>
2021-01-01 16:49:14 +0900

    feat: made favorite list and item

--- lib/screens/favorite_list.dart
+++ lib/screens/favorite_list.dart
@@ -1,8 +1,50 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter/material.dart';
-
 class FavoriteList extends StatelessWidget {
+  final items = ["AAA", "BBB", "CCC"];
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Favorite List'),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return _FavoriteListItem(items[index]);
+        },
+        itemCount: items.length,
+      ),
+    );
+  }
+}
+
+class _FavoriteListItem extends StatelessWidget {
+  final String item;
+
+  _FavoriteListItem(this.item, {Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: LimitedBox(
+        child: Row(
+          children: [
+            IconButton(
+                icon: Icon(
+                  Icons.favorite,
+                  color: Colors.pink,
+                ),
+                onPressed: null),
+            SizedBox(width: 24),
+            Expanded(
+              child: Text(item, style: Theme.of(context).textTheme.bodyText1),
+            ),
+            IconButton(
+                icon: Icon(Icons.auto_delete_sharp, color: Colors.black45),
+                onPressed: null)
+          ],
+        ),
+      ),
+    );
+  }
 }
